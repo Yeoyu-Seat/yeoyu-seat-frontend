a65263eb22f9c1d180a4ab7d8af169877ca44cb2
kyucchoi <kyucchoi@naver.com>
2025-04-07 20:34:34 +0900

    :sparkles: feat: CRM-9 AppBar 컴포넌트 생성

---
+++ src/routes/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+import { StyledText } from '../../../components/StyledText';
+import { TextStyle } from '../../../styles/TextStyle';
+
+export default function Home() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      {/* <h1 className="text-5xl font-bold">여유있냥</h1> */}
+      <StyledText style={TextStyle.B1} className="text-p1">
+        여유있냥
+      </StyledText>
+      <StyledText style={TextStyle.R1} className="text-a">
+        나는 west kite에요~
+      </StyledText>
+    </div>
+  );
+}
