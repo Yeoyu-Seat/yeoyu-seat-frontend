3313c212c61b2b9bff7bf55af91bd065590870af
kyucchoi <kyucchoi@naver.com>
2025-04-13 23:55:23 +0900

    :sparkles: feat: CRM-13 Badge 추가 및 Chips 설정

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,35 @@
+import { cn } from '@/lib/utils';
+import * as React from 'react';
+import { type VariantProps, cva } from 'class-variance-authority';
+
+const badgeVariants = cva(
+  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  {
+    variants: {
+      variant: {
+        default:
+          'border-transparent bg-g9 text-g3 h-8 px-3 py-1 border border-g8 shadow font-normal',
+        secondary:
+          'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        destructive:
+          'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
+        outline: 'text-foreground',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export { Badge, badgeVariants };
